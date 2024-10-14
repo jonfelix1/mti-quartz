36b858ccbffcf2ea51f064ac092fcb9239cb5c4c
Jon Felix Germinian <jon.felix@allofresh.id>
2024-10-14 16:03:14 +0700

    github deploy

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "MTI Digital Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
